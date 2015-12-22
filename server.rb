dfaa3cd0539e093b48010a7498178d022ffa37af
Zlerp <zhpdeals@gmail.com>
2015-12-22 13:53:47 -0500

    Add JS for navigation hamburger and change enter page. Add Contact Page.

--- server.rb
+++ server.rb
@@ -3,3 +3,7 @@ require 'sinatra'
 get '/' do
   File.read(File.join('public', 'index.html')) # YOU NEED THIS!!!!!!
 end
+
+get '/contact' do
+  File.read(File.join('public', 'contact.html')) # YOU NEED THIS!!!!!!
+end
