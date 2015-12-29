1d21752d6826e3fc7e4ee90bbc2730ec8f1f50ab
Zlerp <zhpdeals@gmail.com>
2015-12-28 20:27:41 -0500

    Media page added with some layout:

--- server.rb
+++ server.rb
@@ -4,6 +4,11 @@ get '/' do
   File.read(File.join('public', 'index.html')) # YOU NEED THIS!!!!!!
 end
 
-get '/contact' do
-  File.read(File.join('public', 'contact.html')) # YOU NEED THIS!!!!!!
+get '/media' do
+  File.read(File.join('public', 'media.html')) # YOU NEED THIS!!!!!!
+end
+
+
+get '/about' do
+  File.read(File.join('public', 'about.html')) # YOU NEED THIS!!!!!!
 end
